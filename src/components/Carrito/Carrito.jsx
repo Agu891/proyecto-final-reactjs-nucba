2a016a53d87cebcf5d01e05d40ba20f7e9c03427
Agustin <agu89.1@gmail.com>
2022-08-19 14:09:56 -0300

    cambios finales validaciones para compra

--- src/components/Carrito/Carrito.jsx
+++ src/components/Carrito/Carrito.jsx
@@ -4,13 +4,23 @@ import { formatPrice } from '../../utils/formatPrice';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import * as cartActions from '../../redux/cart/cart-actions';
+
 const Carrito = () => {
+  const currentUser = useSelector((state) => state.user.currentUser);
   const dispatch = useDispatch();
   const hidden = useSelector((state) => state.cart.hidden);
   const cartItems = useSelector((state) => state.cart.cartItems);
   const subTotal = cartItems.reduce((acc, item) => {
     return acc + item.precio * item.quantity;
   }, 0);
+  const clearCart = () => {
+    if (currentUser) {
+      dispatch(cartActions.clearCart());
+      alert('Gracias por su compra');
+    } else {
+      alert('Necesitas crear un usuario para continuar con la compra');
+    }
+  };
   const addToCart = (i) => {
     dispatch(cartActions.addItem(i));
   };
@@ -46,7 +56,7 @@ const Carrito = () => {
         {cartItems.length >= 1 && (
           <>
             <h3 className="totalPrice">Total:{formatPrice(subTotal)}</h3>
-            <button className="finalizarBtn" onClick={console.log('graciass')}>
+            <button className="finalizarBtn" onClick={clearCart}>
               Finalizar compra
             </button>
           </>
