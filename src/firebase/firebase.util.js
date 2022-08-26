5e09d5d6a4af66a8285a51c97d2bd034b8289c11
Agustin <agu89.1@gmail.com>
2022-08-26 12:51:13 -0300

    cambios en dominios firebase2

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -4,7 +4,7 @@ import 'firebase/compat/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB3Gv0tTxoliiP0ZKlWo6VkuUv3MGnG2nE',
-  authDomain: 'proyecto-final-reactjs-nucba.vercel.app/login',
+  authDomain: 'proyecto-final-reactjs-nucba-j155ccjku-agu891.vercel.app',
   projectId: 'proyecto-integrador-reactjs',
   storageBucket: 'proyecto-integrador-reactjs.appspot.com',
   messagingSenderId: '529039508917',
