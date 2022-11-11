dac5f159bedaec0dc1b8d80dcdaa869a4de4c479
Agustin <agu89.1@gmail.com>
2022-11-11 12:52:35 -0300

    subidas las imagenes a la nube

--- src/components/Marcas/Marcas.js
+++ src/components/Marcas/Marcas.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import logoAsus from './img/kisspng-asus-laptop-logo-brand-font-krypton-technologies-device-support-5b7eae6ce474d3.9411941915350288449358.png';
-import logoAMD from './img/png-transparent-advanced-micro-devices-logo-graphics-cards-video-adapters-central-processing-unit-amd-turion-computer-angle-text-trademark.png';
-import logoATI from './img/ati-radeon-vector-logo-11574205010tazreof6xf.png';
-import logoCorsair from './img/png-transparent-corsair-components-computer-keyboard-computer-cases-housings-logo-others-miscellaneous-leaf-computer-keyboard.png';
-import logoNvidia from './img/png-transparent-graphics-cards-video-adapters-nvidia-quadro-logo-geforce-nvidia.png';
-import logoIntel from './img/png-transparent-intel-logo-fujitsu-business-technology-intel-blue-text-trademark.png';
 
 import { BoxLogos, ImgMarcasWrapper } from './MarcasElements';
 const Marcas = () => {
   return (
     <BoxLogos>
       <ImgMarcasWrapper>
-        <img src={logoAMD} alt="logo AMD" />
-        <img src={logoAsus} alt="logo ASUS" />
-        <img src={logoATI} alt="logo ATI" />
-        <img src={logoCorsair} alt="corsair logo" />
-        <img src={logoNvidia} id="logoNvidia" alt="logo nvidia" />
-        <img src={logoIntel} alt="logo intel" />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181665/Products/img/imgMarcas/png-transparent-advanced-micro-devices-logo-graphics-cards-video-adapters-central-processing-unit-amd-turion-computer-angle-text-trademark_l1ett3.png"
+          alt="logo AMD"
+        />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181664/Products/img/imgMarcas/kisspng-asus-laptop-logo-brand-font-krypton-technologies-device-support-5b7eae6ce474d3.9411941915350288449358_khexnw.png"
+          alt="logo ASUS"
+        />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181666/Products/img/imgMarcas/ati-radeon-vector-logo-11574205010tazreof6xf_zekcfa.png"
+          alt="logo ATI"
+        />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181665/Products/img/imgMarcas/png-transparent-corsair-components-computer-keyboard-computer-cases-housings-logo-others-miscellaneous-leaf-computer-keyboard_koxmno.png"
+          alt="corsair logo"
+        />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181665/Products/img/imgMarcas/png-transparent-graphics-cards-video-adapters-nvidia-quadro-logo-geforce-nvidia_eesv4f.png"
+          id="logoNvidia"
+          alt="logo nvidia"
+        />
+        <img
+          src="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181665/Products/img/imgMarcas/png-transparent-intel-logo-fujitsu-business-technology-intel-blue-text-trademark_g4kbjf.png"
+          alt="logo intel"
+        />
       </ImgMarcasWrapper>
     </BoxLogos>
   );
--- src/components/portada/portada.js
+++ src/components/portada/portada.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import { H2Portada, WrapperPortada, WrapperTitulo } from './PortadaElements';
-import FondoPortada from './img/fondoportada.jpg';
 
 const Portada = () => {
   return (
-    <WrapperPortada image={FondoPortada}>
+    <WrapperPortada image="https://res.cloudinary.com/dhfwuqzoz/image/upload/v1668181850/Products/img/imgMarcas/fondoportada_jz5jnz.jpg">
       <WrapperTitulo>
         <H2Portada>
           Sumate a la <span> masterrace</span>
